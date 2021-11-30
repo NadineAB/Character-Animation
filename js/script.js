d34e64a491d2f1666b8bbc9e52b8033597bf8c0a
Nadine Aburumman <nadineaburumman@gmail.com>
2021-11-30 01:46:55 +0000

    Update script.js

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
   renderer,
   camera,
   model, 
-  neck, // Ref to the neck bone in the skeleton
+  neck, 
   waist, // Ref to the waist bone in the skeleton
   possibleAnims, // Animations found in our file
   mixer, // THREE.js animations mixer
